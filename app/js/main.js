2ad16f16a98246779a3b91708f10e09f467d4504
chri911 <delightsome@bigmir.net>
2020-02-14 16:12:43 +0100

    slider added

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,17 @@
 $(function(){
 
-    var mixer = mixitup('.products__inner-box');
     
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+      });
+    $('.product__slider-inner').slick({
+      dots: true,
+      arrows: false,
+      slidesToShow: 4,
+      slidesToScroll: 4
+    }) 
+
+    var mixer = mixitup('.products__inner-box');
 });
