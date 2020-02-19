2c6217d0e501a1d09d1d01f69c72017bc06dc65d
chri911 <delightsome@bigmir.net>
2020-02-19 22:31:48 +0100

    new item upd

--- app/js/main.js
+++ app/js/main.js
@@ -11,7 +11,7 @@ $(function(){
       arrows: false,
       slidesToShow: 4,
       slidesToScroll: 4
-    }) 
+    }); 
 
      $(".js-range-slider").ionRangeSlider({
         type: "double",
@@ -20,6 +20,18 @@ $(function(){
         from: 0,
         to: 600,
         prefix: "$"
+    });
+
+    $('.icon-th-list').on('click', function(){
+      $('.product__item').addClass('list');
+      $('.icon-th-list').addClass('active');
+      $('.icon-th-large').removeClass('active');
+    });
+
+    $('.icon-th-large').on('click', function(){
+      $('.product__item').removeClass('list');
+      $('.icon-th-large').addClass('active');
+      $('.icon-th-list').removeClass('active');      
     });
 
     var mixer = mixitup('.products__inner-box');
